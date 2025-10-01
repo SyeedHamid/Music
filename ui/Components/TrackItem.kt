a7556f69adf413fac70e13da667d0d14fe140947
SyeedHamid <hamidsye98@gmail.com>
2025-10-01 15:26:46 -0400

    Update TrackItem.kt

--- ui/Components/TrackItem.kt
+++ ui/Components/TrackItem.kt
@@ -1,1 +1,54 @@
+class PlayerViewModel(application: Application) : AndroidViewModel(application) {
+    private val player = ExoPlayer.Builder(application).build()
+
+    private val _isPlaying = MutableStateFlow(false)
+    val isPlaying: StateFlow<Boolean> = _isPlaying
+
+    private val _currentPosition = MutableStateFlow(0L)
+    val currentPosition: StateFlow<Long> = _currentPosition
+
+    private val _duration = MutableStateFlow(0L)
+    val duration: StateFlow<Long> = _duration
+
+    init {
+        player.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                _isPlaying.value = isPlaying
+            }
+
+            override fun onPlaybackStateChanged(state: Int) {
+                if (state == Player.STATE_READY) {
+                    _duration.value = player.duration
+                }
+            }
+        })
+
+        viewModelScope.launch {
+            while (true) {
+                _currentPosition.value = player.currentPosition
+                delay(500)
+            }
+        }
+    }
+
+    fun togglePlayPause() {
+        if (player.isPlaying) player.pause() else player.play()
+    }
+
+    fun seekTo(position: Long) {
+        player.seekTo(position)
+    }
+
+    fun play(uri: Uri) {
+        val mediaItem = MediaItem.fromUri(uri)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        player.release()
+    }
+}
 
