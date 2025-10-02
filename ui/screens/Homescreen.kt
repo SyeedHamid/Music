1719504a0ec7ae3bb43dc4f19e92ba945479740b
SyeedHamid <hamidsye98@gmail.com>
2025-10-01 20:01:58 -0400

    Update Homescreen.kt

--- ui/screens/Homescreen.kt
+++ ui/screens/Homescreen.kt
@@ -1,1 +1,35 @@
 
+@Composable
+fun MainScreen(navController: NavHostController, playerViewModel: PlayerViewModel) {
+    Scaffold(
+        bottomBar = {
+            BottomNavigation {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Default.LibraryMusic, contentDescription = null) },
+                    selected = navController.currentDestination?.route == "library",
+                    onClick = { navController.navigate("library") }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Default.QueueMusic, contentDescription = null) },
+                    selected = navController.currentDestination?.route == "queue",
+                    onClick = { navController.navigate("queue") }
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Default.PlayCircleFilled, contentDescription = null) },
+                    selected = navController.currentDestination?.route == "nowplaying",
+                    onClick = { navController.navigate("nowplaying") }
+                )
+            }
+        }
+    ) { padding ->
+        NavHost(
+            navController = navController,
+            startDestination = "library",
+            modifier = Modifier.padding(padding)
+        ) {
+            composable("library") { LibraryScreen(viewModel = playerViewModel.libraryViewModel) }
+            composable("queue") { QueueScreen(viewModel = playerViewModel) }
+            composable("nowplaying") { NowPlayingScreen(viewModel = playerViewModel) }
+        }
+    }
+}
