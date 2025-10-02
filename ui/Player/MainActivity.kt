de5784516feb573a90b35853bbd66883aa72259d
SyeedHamid <hamidsye98@gmail.com>
2025-10-01 20:03:46 -0400

    Update MainActivity.kt

--- ui/Player/MainActivity.kt
+++ ui/Player/MainActivity.kt
@@ -1,1 +1,9 @@
-
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MusicApp()
+        }
+    }
+}
